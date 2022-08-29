5e6dedfacd0e226c04107055206f4dc0e003488b
Sebakor <47070879@est.ort.edu.ar>
2022-08-29 10:47:53 -0300

    Changes

---
+++ ej1.js
@@ -0,0 +1,36 @@
+let alumnos = [
+{
+nombe: "Matias",
+apellido: "Lopez",
+dni: 45987467,
+año: 5,
+curso: "A",
+nota: 7
+},
+
+{
+nombe: "Lautaro",
+apellido: "Perez",
+dni: 45689231,
+año: 5,
+curso: "A",
+nota: 5,
+},
+
+{
+nombe: "Jose",
+apellido: "God",
+dni: 46398777,
+año: 4,
+curso: "A",
+nota: 4,
+},
+{
+nombe: "Matias",
+apellido: "Gar",
+dni: 46802588,
+año: 4,
+curso: "A",
+nota: 8,
+},
+]
