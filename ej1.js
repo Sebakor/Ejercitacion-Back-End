e541e9b4491840d2edf2ea5df1b5fdd2601c127a
Sebakor <47070879@est.ort.edu.ar>
2022-08-30 12:00:21 -0300

    Changes

--- ej1.js
+++ ej1.js
@@ -1,55 +1,66 @@
 let alumnos = [
-{
-nombe: "Matias",
-apellido: "Lopez",
-dni: 45987467,
-año: 5,
-curso: "A",
-nota: 7
-},
+    {
+    nombe: "Matias",
+    apellido: "Lopez",
+    dni: 45987467,
+    año: 5,
+    curso: "A",
+    nota: 7
+    },
+    
+    {
+    nombe: "Lautaro",
+    apellido: "Perez",
+    dni: 45689231,
+    año: 5,
+    curso: "A",
+    nota: 5,
+    },
+    
+    {
+    nombe: "Jose",
+    apellido: "God",
+    dni: 46398777,
+    año: 4,
+    curso: "A",
+    nota: 4,
+    },
+    {
+    nombe: "Matias",
+    apellido: "Gar",
+    dni: 46802588,
+    año: 4,
+    curso: "A",
+    nota: 8,
+    },
+    
+    {
+    nombe: "Jose",
+    apellido: "Fer",
+    dni: 47070879,
+    año: 3,
+    curso: "A",
+    nota: 3,
+    },
+    
+    {
+    nombe: "Fernado",
+    apellido: "Far",
+    dni: 47090786,
+    año: 3,
+    curso: "A",
+    nota: 9,
+    },
+    
+    ]
 
-{
-nombe: "Lautaro",
-apellido: "Perez",
-dni: 45689231,
-año: 5,
-curso: "A",
-nota: 5,
-},
+const capitalizar = (string) => {
+    newLetter = string[0].toUpperCase()
+    newString = newLetter + string.substring(1, string.length)
+    return newString
+}
 
-{
-nombe: "Jose",
-apellido: "God",
-dni: 46398777,
-año: 4,
-curso: "A",
-nota: 4,
-},
-{
-nombe: "Matias",
-apellido: "Gar",
-dni: 46802588,
-año: 4,
-curso: "A",
-nota: 8,
-},
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(capitalizar('hola'))
 
-{
-nombe: "Jose",
-apellido: "Fer",
-dni: 47070879,
-año: 3,
-curso: "A",
-nota: 3,
-},
-
-{
-nombe: "Fernado",
-apellido: "Far",
-dni: 47090786,
-año: 3,
-curso: "A",
-nota: 9,
-},
-
-]
+})
